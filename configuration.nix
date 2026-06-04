c2d51e2266420cad6c4fb8d3ba081ccb5e3f8a14
Kudesn1kGNK <artyom.botvinjev@yandex.ru>
2026-06-04 14:22:57 +0300

    firefox / plasma

--- configuration.nix
+++ configuration.nix
@@ -35,21 +35,6 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "es,ru";
-    variant = ",";
-    options = "grp:ctrl_shift_toggle";
-  };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -76,13 +61,7 @@
   users.users.${username} = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
-    # packages = with pkgs; [
-    #   kdePackages.kate
-    # ];
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;

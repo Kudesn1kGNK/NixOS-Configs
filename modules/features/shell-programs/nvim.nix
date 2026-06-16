a5c159bc33fd310501a169b3f33c7a02a40e64c8
Kudesn1kGNK <artyom.botvinjev@yandex.ru>
2026-06-16 13:44:48 +0300

    Nvim

---
+++ modules/features/shell-programs/nvim.nix
@@ -0,0 +1,31 @@
+{
+  flake.nixosModules.shell-programs = {
+    pkgs,
+    config,
+    ...
+  }: let
+    user = config.preferences.user.name;
+
+    nvimConfig = pkgs.fetchFromGitHub {
+      owner = "Kudesn1kGNK";
+      repo = "LazyVim-Config";
+      rev = "23ff7b2d71ea0822f2d02daf6a62e82a6157fb13";
+      hash = "sha256-9qhQEY9r1mXPdWdB5jdMGTKNkU++HhFIhxu8tw75g6M=";
+    };
+  in {
+    systemd.tmpfiles.rules = [
+      "d /home/${user}/.config/nvim 0755 ${user} users -"
+      "d /root/.config/nvim 0755 root users -"
+
+      "L+ /home/${user}/.config/nvim - - - - ${nvimConfig}"
+      "L+ /root/.config/nvim - - - - ${nvimConfig}"
+    ];
+
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
+}

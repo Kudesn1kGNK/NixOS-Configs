a885057c423e3015e536077ac4dc35741077c7a5
Kudesn1kGNK <artyom.botvinjev@yandex.ru>
2026-06-24 15:40:08 +0300

    update disko

--- disko.nix
+++ disko.nix
@@ -31,5 +31,26 @@
         };
       };
     };
+
+    # disk.data = {
+    #   type = "disk";
+    #   device = "/dev/sdb";
+    #
+    #   content = {
+    #     type = "gpt";
+    #
+    #     partitions = {
+    #       data = {
+    #         size = "100%";
+    #
+    #         content = {
+    #           type = "filesystem";
+    #           format = "ext4";
+    #           mountpoint = "/mnt/sdb";
+    #         };
+    #       };
+    #     };
+    #   };
+    # };
   };
 }

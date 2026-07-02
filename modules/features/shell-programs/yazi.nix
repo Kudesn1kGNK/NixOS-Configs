94c6fe27055acd25e7b2aab0339f50f290b00d25
Kudesn1kGNK <artyom.botvinjev@yandex.ru>
2026-07-02 16:50:30 +0300

    yazi file manager

---
+++ modules/features/shell-programs/yazi.nix
@@ -0,0 +1,23 @@
+{
+  flake.nixosModules.shell-programs = {...}: {
+    programs.yazi = {
+      enable = true;
+      settings = {
+        yazi = {
+          ratio = [
+            1
+            4
+            3
+          ];
+          sort-by = "natural";
+          sort-sensitive = true;
+          sort-reverse = false;
+          sort-dir-first = true;
+          linemode = "none";
+          show-hidden = true;
+          show-symlink = true;
+        };
+      };
+    };
+  };
+}

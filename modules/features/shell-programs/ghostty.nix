e56187e60847b55d28ff0d03674d963288d1acf8
Kudesn1kGNK <artyom.botvinjev@yandex.ru>
2026-07-08 12:25:16 +0300

    ghostty configs

--- modules/features/shell-programs/ghostty.nix
+++ modules/features/shell-programs/ghostty.nix
@@ -1,5 +1,28 @@
 {
-  flake.nixosModules.shell-programs = {pkgs, ...}: {
+  flake.nixosModules.shell-programs = {
+    pkgs,
+    config,
+    ...
+  }: let
+    user = config.preferences.user.name;
+
+    dotfiles = pkgs.fetchFromGitHub {
+      owner = "Kudesn1kGNK";
+      repo = "dotfiles";
+      rev = "fe36262819d7e0befc27eb705bb6ff317fb708ac";
+      hash = "sha256-7wh5RwmjD5AQrjc1QxnUtf9f3Kw2t0cj+aSKPAOmMGY=";
+    };
+  in {
+    home-manager.users.${user} = {
+      xdg.configFile."ghostty".source = "${dotfiles}/ghostty";
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /root/.config 0755 root root -"
+
+      "L+ /root/.config/ghostty - - - - ${dotfiles}/ghostty"
+    ];
+
     environment.systemPackages = with pkgs; [
       ghostty
     ];
